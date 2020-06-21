89ad8b54c3b1d1fcd56b82e8e808aa1f01291bcd
Ciechmar <Ciechmar@o2.pl>
2020-06-21 22:38:27 +0200

    2) Zadanie zprezentacji Lambdy: wynajęcie pokoi

--- src/main/java/zadaniaPrezentacja/AuctionHouse.java
+++ src/main/java/zadaniaPrezentacja/AuctionHouse.java
@@ -41,30 +41,6 @@ public class AuctionHouse {
         return roomsNearCenterList;
 
 
-//        if (roomsForRentList.size() <= 3) {
-//            return roomsForRentList;
-//        } else {
-//
-//            List<Room> roomsNearCenterList;
-//            Room room1 = roomsForRentList.get(1);
-//            Room room2 = roomsForRentList.get(2);
-//            Room room3 = roomsForRentList.get(3);
-//
-//            roomsForRentList.stream().forEach(room -> {
-//                if (room.getOdlegloscOdCentrum() <= room1.getOdlegloscOdCentrum()) {
-//                    room1 = room;
-//                } else if (room.getOdlegloscOdCentrum() <= room2.getOdlegloscOdCentrum()) {
-//                    room2 = room;
-//                } else if (room.getOdlegloscOdCentrum() <= room3.getOdlegloscOdCentrum()) {
-//                    room3 = room;
-//                }
-//
-//
-//        });
-//
-//        roomsNearCenterList = Arrays.asList(room1, room2, room3);
-//        return roomsNearCenterList;
-//    }
 
     }
 
